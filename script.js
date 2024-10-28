59a131683d3defc49fc430c545393b59e0a6913c
Apollo <166862370+z1Apollo@users.noreply.github.com>
2024-10-27 21:39:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tabs = document.querySelectorAll(".tab-btn");
 
-tabs.forEach(tab => tab.addEventListener("click", (tabClicked) => {
+tabs.forEach(tab => tab.addEventListener("click", () => {
     tabs.forEach(tab => tab.classList.remove("active"));
     tab.classList.add("active");
 
